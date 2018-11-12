4301880ee231a04390dabcbb33fb9941334f288a
zehnpaard <zehnpaard@googlemail.com>
2018-11-12 23:19:46 +0900

    Simplify calc to have EInt only and add eval for EInt

--- ch02/calc/calc.ml
+++ ch02/calc/calc.ml
@@ -1,7 +1,6 @@
 type value = [
   | `EInt of int
-  | `EAdd of value * value
-  | `ESub of value * value
-  | `EMul of value * value
-  | `EDiv of value * value
 ]
+
+let eval = function
+  | `EInt n -> n
